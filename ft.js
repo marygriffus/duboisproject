a50e61b00a56566b28e86704d514f2a7a3b043aa
Mary Griffus <mary.e.shultz@gmail.com>
2016-06-28 13:25:36 -0400

    updating to allow for other charts on page

--- ft.js
+++ ft.js
@@ -14,35 +14,37 @@
     }
   }
 
-  var statuses = ["BANKRUPT", "IN DEBT", "CLEARED NOTHING", "CLEARED LESS THAN $10", "CLEARED $10-25", "CLEARED $25-50", "CLEARED $50 OR MORE"]
-  var nums = [3, 168, 53, 27, 15, 13, 13]
+  var ft = {}
 
-  var extraNum = 0
-  for (var i = 2; i < nums.length; i++){
-    extraNum += nums[i];
+  ft.statuses = ["BANKRUPT", "IN DEBT", "CLEARED NOTHING", "CLEARED LESS THAN $10", "CLEARED $10-25", "CLEARED $25-50", "CLEARED $50 OR MORE"]
+  ft.nums = [3, 168, 53, 27, 15, 13, 13]
+
+  ft.extraNum = 0
+  for (var i = 2; i < ft.nums.length; i++){
+    ft.extraNum += ft.nums[i];
   }
 
-  var lineDataA = [
+  ft.lineDataA = [
     {"x": 290, "y": 72},
     {"x": 323, "y": 76},
     {"x": 324, "y": 145},
     {"x": 330, "y": 151}
   ];
-  var lineDataB = [
+  ft.lineDataB = [
     {"x": 330, "y": 151},
     {"x": 324, "y": 157},
     {"x": 323, "y": 226},
     {"x": 290, "y": 230}
   ];
 
-  nums.push(extraNum)
+  ft.nums.push(ft.extraNum)
 
-  var xscale = d3.scale.linear()
-      .domain([0, d3.max(nums)])
+  ft.xscale = d3.scale.linear()
+      .domain([0, d3.max(ft.nums)])
       .range([0, 500])
 
-  var yscale = d3.scale.linear()
-      .domain([0, statuses.length])
+  ft.yscale = d3.scale.linear()
+      .domain([0, ft.statuses.length])
       .range([25, 250]);
 
   var canvas = d3.select('.ftchart')
@@ -52,9 +54,9 @@
   var yAxis = d3.svg.axis();
     yAxis
       .orient("left")
-      .scale(yscale)
+      .scale(ft.yscale)
       .tickSize(0)
-      .tickFormat(function(d, i){ return statuses[i] })
+      .tickFormat(function(d, i){ return ft.statuses[i] })
       .tickValues(d3.range(8))
 
   var y_xis = canvas.append('g')
@@ -62,27 +64,27 @@
     .attr("id", "yaxis")
     .call(yAxis)
 
-  var chart = canvas.append('g')
+  var ftchart = canvas.append('g')
     .attr("transform", "translate(150,0)")
     .attr("id","bars")
     .selectAll("rect")
-    .data(nums)
+    .data(ft.nums)
     .enter()
     .append("rect")
     .attr("height",25)
     .attr("x", function(d, i){
-      if (i==7){ console.log(i); return 180 }
+      if (i==7){ return 180 }
       else { return 10 }
     })
     .attr("y", function(d,i){
       if (i==7){
         return 140;
       } else {
-        return yscale(i)-12.5;
+        return ft.yscale(i)-12.5;
       }
     })
     .style('fill',function(d, i){
-        if (statuses[i] == "BANKRUPT" || statuses[i] == "IN DEBT") {
+        if (ft.statuses[i] == "BANKRUPT" || ft.statuses[i] == "IN DEBT") {
           return "black";
         } else {
           return "#F55A63";
@@ -92,53 +94,53 @@
 
 
   var transit = d3.select("svg").selectAll("rect")
-    .data(nums)
+    .data(ft.nums)
     .transition()
     .duration(500)
-    .attr("width", function(d){ return xscale(d); })
+    .attr("width", function(d){ return ft.xscale(d); })
 
   var transittext = d3.select("#bars")
     .selectAll("text")
-    .data(nums)
+    .data(ft.nums)
     .enter()
     .append("text")
     .attr("x", function(d, i){
       if (i==7){
         return 350;
       } else {
-        return xscale(d)/2 + 6;
+        return ft.xscale(d)/2 + 6;
       }
     })
     .attr('y', function(d,i){
       if (i==7){
         return 157;
       } else {
-        return yscale(i) + 3;
+        return ft.yscale(i) + 3;
       }
     })
     .text(function(d){ return d; })
     .style("fill", function(d, i){
-      if (statuses[i] == "BANKRUPT" || statuses[i] == "IN DEBT") {
+      if (ft.statuses[i] == "BANKRUPT" || ft.statuses[i] == "IN DEBT") {
         return "white";
       } else {
         return "black";
       }
     });
 
-  var lineFunction = d3.svg.line()
+  ft.lineFunction = d3.svg.line()
     .x(function(d) { return d.x; })
     .y(function(d) { return d.y; })
     .interpolate("cardinal");
 
   var lineGraphA = d3.select("svg").append("path")
-    .attr("d", lineFunction(lineDataA))
+    .attr("d", ft.lineFunction(ft.lineDataA))
     .attr("stroke", "black")
     .attr("stroke-width", 1)
     .attr("stroke-cap", "round")
     .attr("fill", "none");
 
   var lineGraphB = d3.select("svg").append("path")
-    .attr("d", lineFunction(lineDataB))
+    .attr("d", ft.lineFunction(ft.lineDataB))
     .attr("stroke", "black")
     .attr("stroke-width", 1)
     .attr("stroke-cap", "round")
